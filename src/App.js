3ebae4bf613d7f2a28f688e5852c4c95d9da50f1
Jerry <maozengs@gmail>
2021-08-09 19:12:53 -0400

    prgin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react"
 
 function App() {
+  const [number1, num1] = useState(0);
+  const [number2, num2] = useState(0);
+  const [total, Total] = useState(number1 + number2);
+  function findtotal() {
+    Total(number1 + number2);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="n">
+      <div className="n">
+        <input
+          type="number"
+          value={number1}
+          onChange={e => num1(+e.target.value)}
+          placeholder="0"
+        />
+        <input
+          type="number"
+          value={number2}
+          onChange={e => num2(+e.target.value)}
+          placeholder="0"
+        />
+      </div>
+      <button onClick={findtotal}>Add The Number!!!</button>
+      <h1>{total}</h1>
     </div>
+      </div>
   );
 }
-
 export default App;
