42efa00245114dbb484afb4102ab901f8bc3e13c
Jerry <maozengs@gmail>
2021-08-22 19:05:01 -0400

    orgin

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
-import logo from './logo.svg';
+import React, { useState , useEffect } from "react";
 import './App.css';
-import React, { useState } from "react"
+import { CheckCircleIcon } from '@heroicons/react/solid';
 
-function App() {
-  const [number1, num1] = useState(0);
-  const [number2, num2] = useState(0);
-  const [total, Total] = useState(number1 + number2);
-  function findtotal() {
-    Total(number1 + number2);
-  }
-  return (
-    <div className="App">
-      <div className="n">
-      <div className="n">
-        <input
-          type="number"
-          value={number1}
-          onChange={e => num1(+e.target.value)}
-          placeholder="0"
-        />
-        <input
-          type="number"
-          value={number2}
-          onChange={e => num2(+e.target.value)}
-          placeholder="0"
-        />
-      </div>
-      <button onClick={findtotal}>Add The Number!!!</button>
-      <h1>{total}</h1>
+export const Time = () => {
+    var [date] = useState(new Date());
+    useEffect(() => {
+        <div className="container">
+        <div className="box">
+            <div className="header"></div>
+
+            <div className="body"></div>
+
+            <div className="todo">
+                <span>Buy New Phone</span>
+                <CheckCircleIcon/>
+
+                <div className="todo">
+                <span>Buy New Phone</span>
+                <CheckCircleIcon/>
+
+                <div className="todo">
+                <span>Buy New Phone</span>
+                <CheckCircleIcon/>
+            </div>
+            <div className="footer">
+                <input type="text" />
+                <button>submit</button>
+            </div>
+        </div>
     </div>
-      </div>
-  );
+  </div>
+  </div>
+
+ });
+    return(
+        <div>
+            <h1> Date : {date.toLocaleDateString()}</h1>
+
+        </div>
+        
+    )
+    
 }
-export default App;
+
+
+export default  Time;
